2b304923f0c4e4c2bd541550075e4cf43f400755
flavia-sudo <kihahuflavia@gmail.com>
2024-04-14 21:00:02 +0300

    Chuka

---
+++ assignment/compare.sh
@@ -0,0 +1,6 @@
+#!/usr/env/bash
+num=101
+if [ $num -gt 100 ] && [ $num -lt 1000 ]:
+then
+echo "That's a big (but not a too big) number!"
+fi
---
+++ assignment/variable.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+my_variable="Hello, world!"
+my_number=42
+echo $my_variable
+echo "The answer is: $my_number"
